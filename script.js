7c58a5ae84abf606ae2af57184dde278a98bb90c
Arvin-Ubindam <161258423+Arvin-Ubindam@users.noreply.github.com>
2025-02-07 20:08:06 +0000

    Done with the layout of the page and just added a bit of js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+
+const items = [
+    { id: 1, name: "Running Shoes", price: 50, category: "shoes", description: "Comfortable running shoes." },
+    { id: 2, name: "Football boots", price: 50, category: "shoes", description: "Comfortable football boots" },
+    { id: 3, name: "Party Dress", price: 100, category: "girls-children", description: "Elegant party dress." },
+    { id: 4, name: "Summer Dress", price: 70, category: "boys-children", description: "Lightweight summer dress." },
+    {id: 5, name: "A Golden Watch", price: 100, category: "watches", description: "A shiny wrist watch for any occasion" },
+];
+
+
 document.getElementById("themeToggle").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
 });
-
